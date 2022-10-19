e65fb4d4dc70e2432b30ce0392abb0395b814286
Noah Braue <nbraue@gmail.com>
2022-10-18 22:24:27 -0400

    more to do on backend

--- app.py
+++ app.py
@@ -1,51 +1,51 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 import yfinance as yf
 import json
 import datetime
 import random as rand
 
 with open("./data/stockdate.json") as infile:
-   stockdate = json.load(infile)
+   stockDate = json.load(infile)
 
-currentDate = "doggy"
-budget = 1000
-existingStocks = []
-ownedStocks = []
+with open("./data/example.json") as infile:
+    exampleData = json.load(infile)
 
-def startGame():
+exampleLabels = [row[0] for row in exampleData]
+exampleValues = [row[1] for row in exampleData]
 
+def getInitialDate():
     currentDate = datetime.date(1970 + round((rand.random() * 10)), 1 + round((rand.random() * 11)), 1 + round((rand.random() * 27)))
     while currentDate.weekday() > 4:
         currentDate = currentDate.replace(day=round((rand.random() * 27)))
+    return currentDate
 
-    for stock in stockdate:
-        if datetime.datetime.strptime(stockdate[stock], '%Y-%m-%d').date() < currentDate:
-            existingStocks.append(stock);
-
-def stockPrompt():
+def getOptions(date):
     options = []
+    existingStocks = []
+    for stock in stockDate:
+        if datetime.datetime.strptime(stockDate[stock], '%Y-%m-%d').date() < date:
+            existingStocks.append(stock)
     for i in range(3):
         options.append(existingStocks[rand.randrange(len(existingStocks))])
     return options
 
 app = Flask(__name__, template_folder='templates', static_folder='statics')
+app.secret_key = "SUPA_SECRET"
 
-@app.route('/', methods=["GET", "POST"])
-def game():
-    data = [
-        ("01-01-2020", 1597),
-        ("02-01-2020", 1456),
-        ("03-01-2020", 1908),
-        ("04-01-2020", 896),
-        ("05-01-2020", 755),
-        ("06-01-2020", 453),
-        ("07-01-2020", 3000),
-        ("08-01-2020", 1293),
-        ("09-01-2020", 1478),
+@app.route('/', methods=["GET"])
+def main():
+    try:
+        return render_template("main.html", activeGame=session['activeGame'], currentDate=session['currentDate'], budget=session['budget'], options=session['options'], exampleValues=exampleValues, exampleLabels=exampleLabels)
+    except KeyError:
+        session['activeGame'] = False
+        return render_template("main.html", activeGame=session['activeGame'], exampleValues=exampleValues, exampleLabels=exampleLabels)
 
-    ]
-    labels = [row[0] for row in data]
-    values = [row[1] for row in data]
-
-    if request.method == "GET":
-        return render_template("main.html", labels=labels, values=values)
+@app.route('/startGame', methods=["POST"])
+def startGame():
+    currentDate = getInitialDate()
+    session['currentDate'] = currentDate.strftime('%d %B %Y')
+    session['options'] = getOptions(currentDate)
+    session['budget'] = 1000
+    session['ownedStocks'] = []
+    session['activeGame'] = True
+    return redirect('/')
